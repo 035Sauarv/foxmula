18a44dd4158cf2df8bacc4cac228e5cc97340962
saurav deshmane <sauravdeshmane095@gmail.com>
2024-03-13 11:24:33 +0530

    commit my project

---
+++ online exam system frontend/src/Components/AdminComponent/AdminSignUp/AdminSignUp.js
@@ -0,0 +1,106 @@
+
+
+import { NavLink, useHistory } from "react-router-dom";
+
+import axios from "axios";
+import { useState } from "react";
+
+
+import style from "./AdminSignUp.module.css";
+
+import baseUrl from "../../baseUrl";
+
+
+function AdminSignUp() {
+
+    const [userData, setUserData] = useState({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+    function onTextFieldChange(e) {
+        setUserData({
+            ...userData,
+            [e.target.name]: e.target.value
+        });
+    }
+
+
+    const [password, setPassword] = useState("");
+
+    function handlePassword(e) {
+        setPassword(e.target.value);
+    }
+
+
+    let history = useHistory();
+
+    async function handleSignup() {
+        // console.log(userData);
+        // console.log(password);
+
+        if (userData.password === password) {
+            await axios.post(`${baseUrl}/admin`, userData);
+            alert("Your account has created");
+            alert("Please Login");
+            history.push("/AdminLogin");
+        }
+        else alert("password did not match");
+    }
+
+
+
+    return (
+        <div id={style.container}>
+
+            <div id={style.formHeading}>
+                <h1>Admin Sign-up</h1>
+                <p>Please complete the form below to register with us</p>
+            </div>
+
+            <div id={style.nameBox}>
+                <label htmlFor="name">Name
+                    <input onChange={(e) => onTextFieldChange(e)}
+                        type="text" name="name" required />
+                </label>
+            </div>
+
+
+            <div id={style.emailBox}>
+                <label htmlFor="email"> Email
+                    <input onChange={(e) => onTextFieldChange(e)}
+                        type="text" name="email" required />
+                </label>
+            </div>
+
+            <div id={style.passwordBox}>
+                <label htmlFor="password"> Password
+                    <input onChange={(e) => onTextFieldChange(e)}
+                        type="password" name="password" required />
+                </label>
+            </div>
+
+
+            <div id={style.confirmPasswordBox}>
+                <label htmlFor="confirmPassword">Confirm Password
+                    <input onChange={(e) => handlePassword(e)}
+                        type="password" name="confirmPassword" required />
+                </label>
+            </div>
+
+
+            {/* <button id={style.signup} onclick="signup()">Sign Up</button> */}
+            <button id={style.signup} onClick={handleSignup} >Sign Up</button>
+
+
+            <div id={style.login}>
+                Have a Account?  <NavLink exact to="/AdminLogin"> Log in</NavLink>
+            </div>
+
+
+        </div>
+    );
+}
+
+export default AdminSignUp;
